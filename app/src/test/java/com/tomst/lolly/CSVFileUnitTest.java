cedb12b4c0bbe44516029bd47c883a74b5ea0da5
Num0Programmer <asajh4@gmail.com>
2024-04-12 00:10:45 -0700

    Added some stuff for testing CSVFile class.

---
+++ app/src/test/java/com/tomst/lolly/CSVFileUnitTest.java
@@ -0,0 +1,11 @@
+package com.tomst.lolly;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+public class CSVFileUnitTest
+{
+}
