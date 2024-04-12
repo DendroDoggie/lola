ba8c166d556f9af1a7e318fb0a9fd82187b1e14b
Num0Programmer <asajh4@gmail.com>
2024-04-12 00:00:26 -0700

    Preliminary commit to see about merging with unit-testing branch.

---
+++ app/src/test/java/com/tomst/lolly/CSVFileUnitTest.java
@@ -0,0 +1,17 @@
+package com.tomst.lolly;
+
+import static org.junit.Assert.assertEquals;
+
+import android.content.Context;
+
+import com.tomst.lolly.core.CSVFile;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+public class CSVFileUnitTest
+{
+}
